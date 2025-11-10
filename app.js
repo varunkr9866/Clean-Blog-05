157dc15089251a3f84f3f851978ecb35e8f807a9
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-10 11:32:59 +0530

    COMPLETED FOLDER STRUCTURE FORCLEAN BLOGPOST

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.listen(4000, () => console.log('Server running on http://localhost:4000'));
