cf2b9faa676663cd254045e33954c59fef411aae
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-11-10 13:32:00 +0530

    CONNECTION ESTABLISHING IN CLEAN BLOGPOST

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql2');
+const mysql_db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"Cluster",
+  database:"blogpost_db"
+})
+
+//Create DB
+
+mysql_db.execute('create database blogpost_db')
+
+//create table in db
+
+mysql_db.execute(`
+  CREATE TABLE USER(
+  ID INT AUTO_INCREMENT ,
+  TITLE VARCHAR(20) NOT NULL,
+  CONTENT VARCHAR(250) NOT NULL UNIQUE
+  );
+    `);
+
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
